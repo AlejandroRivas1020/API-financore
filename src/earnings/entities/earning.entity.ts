78ca49c8dd33108729b9863a8648581fdc4a3515
Scorhenao <samcorhenao@gmail.com>
2024-11-28 16:09:27 -0500

    feat: add the service to validate dates in the earning entity

--- src/earnings/entities/earning.entity.ts
+++ src/earnings/entities/earning.entity.ts
@@ -1,6 +1,9 @@
 import { BaseEntity } from 'src/common/entities/base.entity';
 import { Budget } from './../../badgets/entities/budget.entity';
-import { BeforeInsert, Column, Entity, PrimaryColumn, OneToMany } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryColumn, OneToMany, ManyToOne, JoinColumn, BeforeUpdate } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Inject } from '@nestjs/common';
+import { DatesValidationService } from 'src/common/utils/dates-validation.service';
 
 @Entity('Earnings')
 export class Earning extends BaseEntity {
@@ -9,31 +12,36 @@ export class Earning extends BaseEntity {
   @Column({ type: 'varchar', length: 255, unique: true })
   name: string;
 
-  @Column({ type: 'date', default: () => 'CURRENT_DATE' })
-  startDate?: Date;
+  @Column({ type: 'date', default: () => 'CURRENT_DATE'})
+  startDate: Date;
 
-  @Column({ type: 'date' })
+  @Column({ type: 'date', nullable: true })
   endDate?: Date;
   
   @Column({ type: 'money' })
   generalAmount: number;
-  
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  createdAt: Date;
-  
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  updatedAt: Date;
-  
+
   @OneToMany(() => Budget, (budget) => budget.earning)
   budgets: Budget[]
   
-    @BeforeInsert()
-    setEndDate() {
-      if (this.startDate) {
-        const endDate = new Date(this.startDate);
-        endDate.setDate(endDate.getDate() + 30);
-        this.endDate = endDate;
-      }
+  @ManyToOne(() => User, (user) => user.earnings)
+  @JoinColumn({ name: 'user_id' })
+  user: User
+  
+  constructor(
+    @Inject(DatesValidationService)
+    private readonly datesValidationService: DatesValidationService,
+  ) {
+    super();
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  validateAndSetEndDate() {
+    if (this.startDate) {
+      const result = this.datesValidationService.validateAndSetEndDate(this.startDate, this.endDate);
+      this.startDate = result.startDate;
+      this.endDate = result.endDate;
     }
-    // 
+  }
 }
