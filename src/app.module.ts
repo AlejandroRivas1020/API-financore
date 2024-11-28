42e354831a20e07d95bec2fe0d8c15f0c767022a
Scorhenao <samcorhenao@gmail.com>
2024-11-28 16:11:00 -0500

    feat: add a common module to use the date service and put it in the differents modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { AuthModule } from './auth/auth.module';
 import { BadgetsModule } from './badgets/badgets.module';
 import { EarningsModule } from './earnings/earnings.module';
 import { CategoriesModule } from './categories/categories.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(dbConfig), UsersModule, AuthModule, BadgetsModule, EarningsModule, CategoriesModule],
   controllers: [],
-  providers: [],
+  providers: [
+    CommonModule
+  ],
 })
 export class AppModule {}
--- src/earnings/earnings.module.ts
+++ src/earnings/earnings.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EarningsService } from './earnings.service';
 import { EarningsController } from './earnings.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Earning } from 'src/earnings/entities/earning.entity';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Earning]), CommonModule],
   controllers: [EarningsController],
   providers: [EarningsService],
 })
