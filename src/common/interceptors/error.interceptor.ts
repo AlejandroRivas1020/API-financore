be7aca02626c9684337e21e803bd1381aca9ed01
Scorhenao <samcorhenao@gmail.com>
2024-11-28 22:41:04 -0500

    feat: add interceptor to handle errors

---
+++ src/common/interceptors/error.interceptor.ts
@@ -0,0 +1,64 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  HttpException,
+  HttpStatus,
+  BadRequestException,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class GlobalErrorInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError((error) => {
+        // handle errors of type HttpException
+        if (error instanceof HttpException) {
+          return throwError(
+            () =>
+              new HttpException(
+                {
+                  status: error.getStatus(),
+                  name: error.name,
+                  message: error.message || error.getResponse(),
+                },
+                error.getStatus(),
+              ),
+          );
+        }
+
+        // handle errors of type BadRequestException
+        if (error instanceof BadRequestException) {
+          const response: any = error.getResponse();
+          return throwError(
+            () =>
+              new HttpException(
+                {
+                  status: HttpStatus.BAD_REQUEST,
+                  name: 'ValidationError',
+                  message: response.message || 'Validation failed',
+                },
+                HttpStatus.BAD_REQUEST,
+              ),
+          );
+        }
+
+        // handle other errors
+        return throwError(
+          () =>
+            new HttpException(
+              {
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                name: 'InternalServerError',
+                message: error.message || 'An unexpected error occurred',
+              },
+              HttpStatus.INTERNAL_SERVER_ERROR,
+            ),
+        );
+      }),
+    );
+  }
+}
