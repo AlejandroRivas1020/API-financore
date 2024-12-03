94c9ac1c67f73fb1bc3dac6c4a2db43d84298a1c
Isabela Ortega Vasco <isabela2603ov@gmail.com>
2024-12-02 19:53:17 -0500

    feat: add documentation

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Request } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
+  @ApiOperation({
+    summary: 'Create a new transaction',
+    description:
+      'Create a new transaction with the given data and linking it to a budget',
+  })
   @Post()
   create(@Body() createTransactionDto: CreateTransactionDto) {
     return this.transactionsService.create(createTransactionDto);
   }
 
+  @ApiOperation({
+    summary: 'Get all transactions of user',
+  })
   @Get()
-  findAll() {
+  findAll(@Request() request: any) {
     return this.transactionsService.findAll();
   }
 
--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { BudgetsModule } from '../budgets/budgets.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Transaction]), BudgetsModule],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
